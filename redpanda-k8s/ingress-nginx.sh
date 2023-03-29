a8f0af8361607c5ff3b1208dcdf45fec664429f2
weimeilin79 <weimeilin@gmail.com>
2023-03-29 10:22:47 -0400

    Add Nginx Ingress

--- redpanda-k8s/ingress-nginx.sh
+++ redpanda-k8s/ingress-nginx.sh
@@ -1,2 +1,8 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 kubectl -n ingress-nginx patch deployment ingress-nginx-controller -p '{"spec":{"template":{"spec":{"nodeSelector":null}}}}'
+
+
+while ! kubectl -n ingress-nginx get pod -l app.kubernetes.io/component=controller | grep -w "Running"; do
+  echo "."
+  sleep 1;
+done
