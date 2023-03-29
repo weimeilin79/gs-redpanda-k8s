21ec4c80163e6d4f93a175e5f09d901acaad8a80
weimeilin79 <weimeilin@gmail.com>
2023-03-29 11:37:55 -0400

    update node selector to be always control plane

--- redpanda-k8s/ingress-nginx.sh
+++ redpanda-k8s/ingress-nginx.sh
@@ -1,8 +1,8 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl -n ingress-nginx patch deployment ingress-nginx-controller -p '{"spec":{"template":{"spec":{"nodeSelector":null}}}}'
+kubectl -n ingress-nginx patch deployment ingress-nginx-controller -p '{"spec":{"template":{"spec":{"nodeSelector":{"kubernetes.io/hostname":"controlplane"}}}}}'
 
 
 while ! kubectl -n ingress-nginx get pod -l app.kubernetes.io/component=controller | grep -w "Running"; do
-  echo "."
+  echo "Waiting for Nginx ingress to get ready......"
   sleep 1;
 done
