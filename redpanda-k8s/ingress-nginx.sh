5a6fb3024f08c11b6f515a9093c07dcba61a71cb
weimeilin79 <weimeilin@gmail.com>
2023-03-29 11:49:48 -0400

    update node selector to not bound to any node

--- redpanda-k8s/ingress-nginx.sh
+++ redpanda-k8s/ingress-nginx.sh
@@ -1,8 +1,8 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl -n ingress-nginx patch deployment ingress-nginx-controller -p '{"spec":{"template":{"spec":{"nodeSelector":{"kubernetes.io/hostname":"controlplane"}}}}}'
+kubectl -n ingress-nginx patch deployment ingress-nginx-controller -p '{"spec":{"template":{"spec":{"nodeSelector":null}}}}'
 
-
+echo -n "Waiting for Nginx ingress to get ready...."
 while ! kubectl -n ingress-nginx get pod -l app.kubernetes.io/component=controller | grep -w "Running"; do
-  echo "Waiting for Nginx ingress to get ready......"
+  echo  -n ".."
   sleep 1;
 done
