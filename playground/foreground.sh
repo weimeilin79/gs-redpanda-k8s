30ff563cc8b311134a6a33d5ae88d4a4cc050a13
weimeilin79 <weimeilin@gmail.com>
2023-07-05 13:28:23 -0400

    check for status code 200 for rp console

--- playground/foreground.sh
+++ playground/foreground.sh
@@ -47,7 +47,7 @@ helm upgrade --install redpanda redpanda/redpanda \
     --namespace redpanda --create-namespace \
     --values console.yaml --reuse-values
 
-while [ ! "$(kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- curl -s -o /dev/null -w '%{http_code}' http://redpanda-console:8080)" -eq 200 ] ; do
+while [ ! "$(kubectl -n redpanda exec -ti redpanda-0 -c redpanda -- curl -s -o /dev/null -w '%{http_code}' http://redpanda-console:8080)" == '200' ] ; do
   echo  -n ".."
   sleep 1;
 done 
