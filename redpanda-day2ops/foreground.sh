d859ac7977efb2ecc9f8ad225c9c407f47f2b7f9
weimeilin79 <weimeilin@gmail.com>
2023-08-18 16:11:27 -0400

    add content

--- redpanda-day2ops/foreground.sh
+++ redpanda-day2ops/foreground.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-git clone https://github.com/redpanda-data/observability.git
+# start 3 initial brokers
+docker-compose \
+-p 1-commissioning-brokers \
+-f compose.redpanda-0.yaml \
+-f compose.redpanda-1.yaml \
+-f compose.redpanda-2.yaml \
+-f compose.console.yaml \
+up -d
+# create topic
+rpk topic create log -p 3 -r 3
+# generate data
+./generate-data.sh &
 
-cd observability/demo
-
-docker-compose pull
-
-docker-compose up -d 
