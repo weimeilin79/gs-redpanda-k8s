cb0f850694b1821126fff7d6da7b765b3151ccba
weimeilin79 <weimeilin@gmail.com>
2023-10-11 08:31:46 -0400

    avoid pulling same image at the same time

--- mm2-1-replication/foreground.sh
+++ mm2-1-replication/foreground.sh
@@ -2,7 +2,7 @@
 
 # build and pull the images while the user is reading the intro
 cd /root
-docker-compose -f docker-compose-clients.yaml pull
+docker pull weimeilin/mm2consumer:latest
 docker-compose -f docker-compose-rp.yaml pull
 docker-compose -f docker-compose-kafka.yaml pull
 
