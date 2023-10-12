c1c5de7a06081216250beaa568cab4a13b0a179e
weimeilin79 <weimeilin@gmail.com>
2023-10-12 15:59:21 -0400

    preload jars

--- mm2-2-schema-registry/background.sh
+++ mm2-2-schema-registry/background.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-sudo apt update
+
 sudo apt install -y default-jdk
 sudo apt install -y maven 
 
 docker-compose -f docker-compose-rp.yaml pull
+
+cd /root/quarkus-apps/avro-schema-producer
+./mvnw install
+
+cd /root/quarkus-apps/avro-schema-consumer
+./mvnw install
